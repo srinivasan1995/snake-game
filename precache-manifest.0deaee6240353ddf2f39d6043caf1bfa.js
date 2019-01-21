b5c201812f01c6872af1214aa4d8f0dd3d59a7e2
srinivasan1995 <r.srinivasan1995@gmail.com>
2019-01-21 22:15:17 +0530

    Updates

---
+++ precache-manifest.0deaee6240353ddf2f39d6043caf1bfa.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "351fc99354337a05b0be",
+    "url": "/snake-game/static/js/runtime~main.351fc993.js"
+  },
+  {
+    "revision": "77f7c85b85e5a32c5ca4",
+    "url": "/snake-game/static/js/main.77f7c85b.chunk.js"
+  },
+  {
+    "revision": "24b15b5346dfe6e9e738",
+    "url": "/snake-game/static/js/1.24b15b53.chunk.js"
+  },
+  {
+    "revision": "77f7c85b85e5a32c5ca4",
+    "url": "/snake-game/static/css/main.92b41578.chunk.css"
+  },
+  {
+    "revision": "16b89ee7b329ca3060cf11ae670b8f8c",
+    "url": "/snake-game/index.html"
+  }
+];
